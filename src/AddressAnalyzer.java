cc6654f2073028d51096e43c129a5b0407dc6ed9
jgonsalves21 <42977061+jgonsalves21@users.noreply.github.com>
2018-11-05 13:37:57 -0500

    Add files via upload

---
+++ src/AddressAnalyzer.java
@@ -0,0 +1,22 @@
+
+import java.util.ArrayList;
+import java.io.File;
+import java.io.FileNotFoundException;
+import  java.util.Scanner;
+public class AddressAnalyzer {
+
+	public static void main(String[] args) throws FileNotFoundException 
+	{
+		Scanner file = new Scanner(new File("GettysburgAddress.txt"));
+		ArrayList<String> words = new ArrayList<String>();
+		int numWords = 0;
+		while (file.hasNext())
+		{
+			
+		}
+		
+		
+
+	}
+
+}
