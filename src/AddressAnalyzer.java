17dd563999b0c88b8eaf2166da6d39f3a6a2763b
jgonsalves21 <42977061+jgonsalves21@users.noreply.github.com>
2018-11-08 13:37:19 -0500

    Add files via upload

--- src/AddressAnalyzer.java
+++ src/AddressAnalyzer.java
@@ -10,12 +10,28 @@ public class AddressAnalyzer {
 		Scanner file = new Scanner(new File("GettysburgAddress.txt"));
 		ArrayList<String> words = new ArrayList<String>();
 		int numWords = 0;
-		while (file.hasNext())
+		String longWord = file.next();
+		double aveLength = 0;
+		
+		for (int a : words)
 		{
 			
 		}
+		while (file.hasNext())
+		{
+			String word = file.next();
+			numWords ++;
+			if (word.length() > longWord.length())
+			{
+				longWord = word;
+			}
+			aveLength = aveLength + word.length();
+		}
+		aveLength = aveLength/numWords;
 		
-		
+		System.out.println(longWord);
+		System.out.println(aveLength);
+		System.out.println(numWords);
 
 	}
 
