e6073cdad4a137f3974a236016bc9c7837cd99a5
jgonsalves21 <42977061+jgonsalves21@users.noreply.github.com>
2018-11-08 19:54:17 -0500

    Add files via upload

--- src/AddressAnalyzer.java
+++ src/AddressAnalyzer.java
@@ -1,4 +1,10 @@
-
+/**
+ * 
+ * @author Joshua Gonsalves
+ * Gettysburg Address
+ * 
+ */
+import java.util.Scanner;
 import java.util.ArrayList;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -7,16 +13,13 @@ public class AddressAnalyzer {
 
 	public static void main(String[] args) throws FileNotFoundException 
 	{
+		Scanner in = new Scanner(System.in);
 		Scanner file = new Scanner(new File("GettysburgAddress.txt"));
 		ArrayList<String> words = new ArrayList<String>();
 		int numWords = 0;
 		String longWord = file.next();
 		double aveLength = 0;
 		
-		for (int a : words)
-		{
-			
-		}
 		while (file.hasNext())
 		{
 			String word = file.next();
@@ -29,9 +32,9 @@ public class AddressAnalyzer {
 		}
 		aveLength = aveLength/numWords;
 		
-		System.out.println(longWord);
-		System.out.println(aveLength);
-		System.out.println(numWords);
+		System.out.println("The longest word is " + longWord + ".");
+		System.out.println("The average word length is " + aveLength + ".");
+		System.out.println("The number of words is " + numWords + ".");
 
 	}
 
